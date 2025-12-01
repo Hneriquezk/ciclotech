2cf9ddfcb5bd7b8616bcac1cf45f8b83e6ec21f4
Henriquezk <henriquelimasts@gmail.com>
2025-12-01 07:39:02 -0300

    Delete html directory

--- html/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-// auth.php
-require_once __DIR__ . '/sessao.php';
-function require_login() {
-    if (empty($_SESSION['usuario']) || empty($_SESSION['usuario']['id'])) {
-        header('Location: login.php');
-        exit;
-    }
-}
